d0b34fb1205b3df846be8ac21685835309c53ac0
Maksim <menshikovma@yandex.ru>
2023-09-14 12:13:58 +0500

    Second commit

--- src/main/java/ru/itlearn/DemoSpringBoot/hello/HelloController.java
+++ src/main/java/ru/itlearn/DemoSpringBoot/hello/HelloController.java
@@ -1,33 +1,36 @@
 package ru.itlearn.DemoSpringBoot.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import java.util.*;
 
+import java.util.*;
 
 @RestController
 public class HelloController {
-    private List<String> anyWeb = new ArrayList<>();
-    private Map<String, String> anyMap = new HashMap<>();
+
+    private List<String> strings = new ArrayList<>();
+    private Map<String, String> stringHashMap = new HashMap<>();
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name",
             defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+
     }
+
     /*метод updateArrayList(String s), который обрабатывает GET апрос на url “/update-array” принимает аргумент
     и записывает его в ArrayList<String >, реализуйте логику - если это первый вызов метода,
     то создается пустой ArrayList, если он не пустой, то туда записывается значение.*/
     @GetMapping("/update-array/{value}")
     public String updateArrayList(@PathVariable("value") String value) {
         String outMessage;
-        if (anyWeb.isEmpty()) {
-            anyWeb.add(value);
+        if (strings.isEmpty()) {
+            strings.add(value);
             outMessage ="пустой Array List создан и ваши данные записаны";
         } else {
-            anyWeb.add(value);
+            strings.add(value);
             outMessage = value + ": добавлен в Array List";
         }
         return outMessage;
@@ -37,7 +40,7 @@ public class HelloController {
     хранящиеся в ArrayList <String >*/
     @GetMapping("/show-array")
     public List<String> showArrayList() {
-        return anyWeb;
+        return strings;
 
     }
 
@@ -47,12 +50,12 @@ public class HelloController {
     @GetMapping("/update-map/{key}/{value}")
     public Map<String, String> updateHashMap(@PathVariable("key") String key,
                                              @PathVariable("value") String value) {
-        if (anyMap.isEmpty()) {
-            anyMap.put(key, value);
+        if (stringHashMap.isEmpty()) {
+            stringHashMap.put(key, value);
         } else {
-            anyMap.put(key, value);
+            stringHashMap.put(key, value);
         }
-        return anyMap;
+        return stringHashMap;
     }
 
     /*GET метод showHashMap (), который по url “/show-map” возвращается все элементы
@@ -60,15 +63,15 @@ public class HelloController {
     @GetMapping("/show-map")
     public Map<String, String> showHashMap() {
 
-        return anyMap;
+        return stringHashMap;
     }
 
     /*метод showAllLenght (), который обрабатывает GET апрос на url “/show-all-lenght” и возвращает текст,
     в котором указано количество элементов в ArrayList и HashMap*/
     @GetMapping("/show-all-length")
     public String showAllLength() {
-        int arrayListLength = anyWeb.size();
-        int hashMapLength = anyMap.size();
+        int arrayListLength = strings.size();
+        int hashMapLength = stringHashMap.size();
         return "Количество элементов в ArrayList: " + arrayListLength + "<br><hr>" +
                 "Количество элементов в HashMap: " + hashMapLength;
     }
